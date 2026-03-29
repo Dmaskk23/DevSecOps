764147b303bc30036ed41d863803c716b081573a
Damaskk23 <ye.ye.drahomyretskyi.fitis22@chdtu.edu.ua>
2026-03-29 14:38:33 +0300

    fix one more

--- src/app.py
+++ src/app.py
@@ -33,4 +33,4 @@ def convert():
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port) # nosec B104
+    app.run(host="0.0.0.0", port=port)  # nosec B104
